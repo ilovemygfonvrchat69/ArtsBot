25e0023d85eec4e83523ca069b96e1b6114573e7
ilovemygfonvrchat69 <ilovemygfonvrchat69@gmail.com>
2026-05-22 16:15:50 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "3 Watching The Server",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#FF00EF", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
